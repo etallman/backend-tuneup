d89e622b0b7d2b7d284bcb9360bb0d0e8aad0ae2
Eileen <eileen.tallman@gmail.com>
2019-07-09 13:44:03 -0400

    Completed

--- tuneup.py
+++ tuneup.py
@@ -12,21 +12,21 @@ import logging
 
 
 def profile(func):
-        """A function that can be used as a decorator to measure performance"""
-        # You need to understand how decorators are constructed and used.
-        # Be sure to review the lesson material on decorators, they are used
-        # extensively in Django and Flask.
-        @functools.wraps(func)
-        def inner(*args, **kwargs):
-                profiler = cProfile.Profile()
-                profiler.enable()
-                result = func(*args, **kwargs)
-                profiler.disable()
-                ps = pstats.Stats(profiler).strip_dirs().sort_stats('cumulative')
-                ps.print_stats(10)
-                return result
-        return inner
-        #raise NotImplementedError("Complete this decorator function")
+    """A function that can be used as a decorator to measure performance"""
+    # You need to understand how decorators are constructed and used.
+    # Be sure to review the lesson material on decorators, they are used
+    # extensively in Django and Flask.
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        result = func(*args, **kwargs)
+        profiler.disable()
+        ps = pstats.Stats(profiler).strip_dirs().sort_stats('cumulative')
+        ps.print_stats(10)
+        return result
+    return inner
+    #raise NotImplementedError("Complete this decorator function")
 
 
 def read_movies(src):
@@ -37,29 +37,26 @@ def read_movies(src):
 
 
 def is_duplicate(title, movies):
-    """returns True if title is within movies list"""
-    duplicates = []
-
-    for movie in movies:
-        if movie == title:
-            duplicates.append(movie)
-    return duplicates
-
-                        
+        """returns True if title is within movies list"""
+        # title=title.lower()
+        # for movie in movies:
+        
+        if title in movies:
+                return True
+        return False
+        
 
 @profile
 def find_duplicate_movies(src):
-    """Returns a list of duplicate movies from a src list"""
-    movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        # for movie in movies:
-        #         if movie.lowercase() == title.lowercase():
-        #                 duplicates.append(movie)
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
-    return duplicates
+        """Returns a list of duplicate movies from a src list"""
+        movies = read_movies(src)
+        movies = [movie.lower() for movie in movies]
+        duplicates = []
+        while movies:
+                movie = movies.pop()
+                if is_duplicate(movie, movies):
+                        duplicates.append(movie)
+        return duplicates
 
 
 def timeit_helper():
@@ -67,7 +64,8 @@ def timeit_helper():
     t = timeit.Timer(stmt='main', setup='main')
     result = t.repeat(repeat=7, number=5)
     min_result = min(result)
-    average_result = (('Best time across 7 repeats of 5 runs per repeat: {} sec').format(min_result))
+    average_result = (
+        ('Best time across 7 repeats of 5 runs per repeat: {} sec').format(min_result))
     return average_result
 
 
@@ -76,7 +74,8 @@ def main():
     result = find_duplicate_movies('movies.txt')
     print('Found {} duplicate movies:'.format(len(result)))
     print('\n'.join(result))
-    #print(timeit_helper())
+    # print(timeit_helper())
+
 
 if __name__ == '__main__':
     main()
